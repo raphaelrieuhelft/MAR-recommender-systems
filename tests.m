22705ffd066c8268966a177fc968bae643fcde10
dianegalloiswong <diane.gallois-wong@ens.fr>
2015-01-24 01:41:49 +0100

    Tests for different p

--- tests.m
+++ tests.m
@@ -12,7 +12,11 @@ warning('off','MATLAB:nearlySingularMatrix')
 
 M = load('recommendMatrix.txt');
 p=0.2;
-iter=1;
+iter=3;
+
+nprobas = 4;
+probas = linspace(0,1,nprobas+2);
+probas = probas(2:nprobas+1);
 
 algos = {
     'Witness', @witness;
@@ -33,22 +37,34 @@ algos = {
     };
 [nalgos,~] = size(algos);
 
-MSEs = cellfun(@(algo) MSE(M,p,iter,algo), algos(:,2));
-MSEs = num2cell(MSEs);
+results = cell(nalgos+1,nprobas+1);
+results(1,1) = {'Algorithme  \  p'};
+results(2:nalgos+1,1) = algos(:,1);
+results(1,2:nprobas+1) = num2cell(probas);
+for i = 1:nprobas
+    MSEs = cellfun(@(algo) MSE(M,probas(i),iter,algo), algos(:,2));
+    MSEs = num2cell(MSEs);
+    results(2:nalgos+1,i+1) = MSEs;
+end
+results
 
-% notSVD = logical([1,1,1,1,0,0,0,1,1,1,1]);
-% notSVDalgos = algos(:,2);
-% notSVDalgos = notSVDalgos(notSVD);
-% M1 = randObserve(M,0.1);
-% MAEs = cellfun(@(algo) MAE(M1,algo), notSVDalgos);
-% MAEs = num2cell(MAEs);
-% t1 = cell(nalgos,1);
-% t1(notSVD) = MAEs;
-% MAEs = t1;
 
-results = [algos(:,1), MSEs, 
-    %MAEs
-    ]
+% MSEs = cellfun(@(algo) MSE(M,p,iter,algo), algos(:,2));
+% MSEs = num2cell(MSEs);
+% 
+% % notSVD = logical([1,1,1,1,0,0,0,1,1,1,1]);
+% % notSVDalgos = algos(:,2);
+% % notSVDalgos = notSVDalgos(notSVD);
+% % M1 = randObserve(M,0.1);
+% % MAEs = cellfun(@(algo) MAE(M1,algo), notSVDalgos);
+% % MAEs = num2cell(MAEs);
+% % t1 = cell(nalgos,1);
+% % t1(notSVD) = MAEs;
+% % MAEs = t1;
+% 
+% results = [algos(:,1), MSEs, 
+%     %MAEs
+%     ]
 % plainErr = MSE(M,p, iter, @plainSVD)
 % shiftErr = MSE(M,p, iter, @shiftSVD)
 % unbiasErr = MSE(M, p, iter, @unbiasedSVD)
