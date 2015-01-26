f46a79163f9d04390e8f0869ca9342ccd374a825
dianegalloiswong <diane.gallois-wong@ens.fr>
2015-01-26 02:43:16 +0100

    Change unbias

--- unbias.m
+++ unbias.m
@@ -4,30 +4,24 @@ function [ Mnorm, b, m ] = unbias( M )
 [n,p]=size(M);
 omega=numel(M(isnan(M)==0));
 A = zeros(omega, n+p);
-c = M(isnan(M)==0);
+c = M(~isnan(M));
 m=mean(c);
 c= c - m;
-count=0;
-Mnorm = zeros(n,p);
-for j=1:p
-    for i=1:n
-        if (isnan(M(i,j)))
-        else
-            count=count+1;
-            A(count, i)=1;
-            A(count, n+j)=1;
-        end
+
+[row,col] = find(~isnan(M));
+count = numel(row);
+ignore = arrayfun(@FillA,row,col,(1:count)');
+    function output = FillA(i,j,k)
+        assert(not(isnan(M(i,j))))
+        A(k, i)=1;
+        A(k, n+j)=1;
+        output = 0;
     end
-end
-assert(count==numel(c));
+
 b=(A'*A)\(A'*c);
-for j=1:p
-    for i=1:n
-        if (isnan(M(i,j)))
-        else
-            Mnorm(i,j)=M(i,j)-(m+b(i)+b(n+j));
-        end
-    end
-end
+
+Mnorm = M - ( m + repmat(b(1:n),1,p) + repmat(b(n+1:n+p)',n,1) );
+Mnorm(isnan(Mnorm)) = 0;
+
 end
 
