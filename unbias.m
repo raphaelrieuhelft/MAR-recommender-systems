18015f005c795553d46cac3d88763996466af169
dianegalloiswong <diane.gallois-wong@ens.fr>
2015-01-26 03:27:50 +0100

    Make unbias really more efficient
    Enfin :D
    45s -> 3s

--- unbias.m
+++ unbias.m
@@ -2,25 +2,17 @@ function [ Mnorm, b, m ] = unbias( M )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 [n,p]=size(M);
-omega=numel(M(isnan(M)==0));
-A = zeros(omega, n+p);
 c = M(~isnan(M));
 m=mean(c);
 c= c - m;
 
-count=0;
-Mnorm = zeros(n,p);
-for j=1:p
-    for i=1:n
-        if (isnan(M(i,j)))
-        else
-            count=count+1;
-            A(count, i)=1;
-            A(count, n+j)=1;
-        end
-    end
-end
-assert(count==numel(c));
+[i,j,s] = find(~isnan(M));
+k = 1:numel(i);
+
+A1 = sparse(k,i,s);
+A2 = sparse(k,j,s);
+A = double([A1,A2]);
+
 b=(A'*A)\(A'*c);
 
 Mnorm = M - ( m + repmat(b(1:n),1,p) + repmat(b(n+1:n+p)',n,1) );
